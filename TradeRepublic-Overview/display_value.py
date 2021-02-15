18cc0f0bee569e6f937bb6ef0c0d660013313943
Giebisch <rafael@giebisch-mail.de>
2021-02-15 15:51:43 +0100

    Read positions.csv

--- TradeRepublic-Overview/display_value.py
+++ TradeRepublic-Overview/display_value.py
@@ -1,5 +1,24 @@
-def display_value():
-    positions = get_positions()
+import os
+from csv import reader
+
+def get_positions(folder):
+    positions = []
+    try:
+        with open(os.path.join(folder, "positions.csv")) as infile:
+            csv = reader(infile, delimiter=";")
+            next(csv)
+            for line in csv:
+                dic = dict()
+                dic["isin"] = line[0]
+                dic["quantity"] = float(line[1])
+                dic["price"] = float(line[2])
+                positions.append(dic)
+    except:
+        raise Exception("No positions.csv file found, parse your PDFs first.")
+    return positions
+
+def display_value(folder):
+    positions = get_positions(folder)
     value = get_current_value(positions)
     print_formatted_value(value, "|")
 
