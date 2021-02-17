00db55c2b71e80f34c6cfa8e9752409e8b3a56fb
Giebisch <rafael@giebisch-mail.de>
2021-02-17 12:01:37 +0100

    Added setup.py and tests folder

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="traderep", # Replace with your own username
+    version="0.0.1",
+    author="Giebisch",
+    author_email="rafael@giebisch-mail.de",
+    description="Application for parsing positions and \
+        displaying their current market value",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Giebisch/TradeRepublic-Overview",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
